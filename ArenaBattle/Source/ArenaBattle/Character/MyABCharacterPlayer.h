21b41897c36cca0b3252f6bd2a5d8f5074c09643
heajun <heajun35@gmail.com>
2026-01-25 19:36:29 +0900

    Feat: 1강 언리얼 엔진 게임 제작 기초

---
+++ ArenaBattle/Source/ArenaBattle/Character/MyABCharacterPlayer.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/ABCharacterBase.h"
+#include "MyABCharacterPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARENABATTLE_API AMyABCharacterPlayer : public AABCharacterBase
+{
+	GENERATED_BODY()
+	
+};
