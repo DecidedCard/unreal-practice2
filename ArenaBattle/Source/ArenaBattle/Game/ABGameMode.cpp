9f2393a5142fda9e1282db611c3bc470fc1d16ef
heajun <heajun35@gmail.com>
2026-02-09 04:06:11 +0900

    Feat: 2강 캐릭터와 입력 시스템

--- ArenaBattle/Source/ArenaBattle/Game/ABGameMode.cpp
+++ ArenaBattle/Source/ArenaBattle/Game/ABGameMode.cpp
@@ -6,6 +6,12 @@
 AABGameMode::AABGameMode()
 {
 	// DefaultPawnClass
+	//static ConstructorHelpers::FClassFinder<APawn> ThirdPersonClassRef(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter.BP_ThirdPersonCharacter_C"));
+	//if (ThirdPersonClassRef.Class)
+	//{
+	//	DefaultPawnClass = ThirdPersonClassRef.Class;
+	//}
+
 	static ConstructorHelpers::FClassFinder<APawn> DefaultPawnClassRef(TEXT("/Script/ArenaBattle.ABCharacterPlayer"));
 	if (DefaultPawnClassRef.Class)
 	{
