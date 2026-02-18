94de04e16ef775a1e0e656c5b5609212f8ab3fb7
heajun <heajun35@gmail.com>
2026-02-18 23:06:35 +0900

    Feat: 6강 캐릭터 공격 판정

---
+++ ArenaBattle/Source/ArenaBattle/Physics/ABColision.h
@@ -0,0 +1,8 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#define CPROFILE_ABCAPSULE TEXT("ABCapsule")
+#define CPROFILE_ABTRIGGER TEXT("ABTrigger")
+#define CPROFILE_ABACTION ECC_GameTraceChannel1
