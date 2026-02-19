e48d8f86307deaeaba60469ed93caa6b55830ef6
DecidedCard <qube7089@naver.com>
2026-02-20 03:00:36 +0900

    Feat: 8강 아이템 시스템 학습 중

---
+++ ArenaBattle/Source/ArenaBattle/Item/ABItemBox.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Item/ABItemBox.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Particles/ParticleSystemComponent.h"
+#include "Physics/ABColision.h"
+
+// Sets default values
+AABItemBox::AABItemBox()
+{
+	Trigger = CreateDefaultSubobject<UBoxComponent>(TEXT("TriggerBox"));
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Effect = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("Effect"));
+
+	RootComponent = Trigger;
+	Mesh->SetupAttachment(Trigger);
+	Effect->SetupAttachment(Trigger);
+
+	Trigger->SetCollisionProfileName(CPROFILE_ABTRIGGER); // Collision Profile Setup
+	Trigger->SetBoxExtent(FVector(40.0f, 42.0f, 30.0f)); // Box Extent Setup
+	Trigger->OnComponentBeginOverlap.AddDynamic(this, &AABItemBox::OnOverlapBegin); // Overlap Event Binding
+
+	// Mesh Setup
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> BoxMeshRef(TEXT("/Script/Engine.StaticMesh'/Game/ArenaBattle/Environment/Props/SM_Env_Breakables_Box1.SM_Env_Breakables_Box1'"));
+	if (BoxMeshRef.Object)
+	{
+		Mesh->SetStaticMesh(BoxMeshRef.Object);
+	}
+	Mesh->SetRelativeLocation(FVector(0.0f, -3.5f, -30.0f));
+	Mesh->SetCollisionProfileName(TEXT("NoCollision"));
+
+	// Effect Setup
+	static ConstructorHelpers::FObjectFinder<UParticleSystem> EffectRef(TEXT("/Script/Engine.ParticleSystem'/Game/ArenaBattle/Effect/P_TreasureChest_Open_Mesh.P_TreasureChest_Open_Mesh'"));
+	if (EffectRef.Object)
+	{
+		Effect->SetTemplate(EffectRef.Object);
+		Effect->bAutoActivate = false;
+	}
+}
+
+void AABItemBox::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepHitResult)
+{
+	Effect->Activate(true);
+	Mesh->SetHiddenInGame(true);
+	SetActorEnableCollision(false);
+	Effect->OnSystemFinished.AddDynamic(this, &AABItemBox::OnEffectFinished);
+
+}
+
+void AABItemBox::OnEffectFinished(UParticleSystemComponent* PSystem)
+{
+	Destroy();
+}
+
+
+
